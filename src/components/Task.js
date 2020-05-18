6493e71d6aaefd0fe7f769e74cd1edc264c3e377
Lexa <lexaraea@gmail.com>
2020-05-18 13:32:05 -0400

    Add icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,11 +5,11 @@ const Task = () => {
         <li className="list-item">
             <span>Task Title</span>
             <div>
-                <button className="btn-delete task-btn">delete
-                    <i className="fas-fa-trash-alt"></i>
+                <button className="btn-delete task-btn">
+                    <i className="fas fa-trash"></i>
                 </button>
-                <button className="btn-edit task-btn">edit
-                    <i className="fas-fa-pen"></i>
+                <button className="btn-edit task-btn">
+                    <i className="fas fa-pen"></i>
                 </button>
             </div>
         </li>
